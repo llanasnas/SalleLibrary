b2fd5e23c51bb6bbfba7576281bb505b4a0bc6ed
llanas <llanasnas@gmail.com>
2018-02-14 19:10:06 +0100

    initial commit

---
+++ app/src/main/java/com/example/alumne/sallelibrary/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.alumne.sallelibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+
+
+
+    }
+}
