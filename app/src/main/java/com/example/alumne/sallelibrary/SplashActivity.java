f2e1ba3a26afab70d9c7ad23eafd34719dc314fa
imNever <tehnever@gmail.com>
2018-02-21 19:35:22 +0100

    SplashActivity creada (la habiamos creado mal) :(

--- app/src/main/java/com/example/alumne/sallelibrary/SplashActivity.java
+++ app/src/main/java/com/example/alumne/sallelibrary/SplashActivity.java
@@ -1,8 +1,13 @@
 package com.example.alumne.sallelibrary;
 
-/**
- * Created by Alumne on 21/02/2018.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class SplashActivity {
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
 }
